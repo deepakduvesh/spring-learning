cf168c0c505ad1aa16251e8618b0a103a3fe125b
deepakduvesh <121632554+deepakduvesh@users.noreply.github.com>
2025-06-01 09:01:38 +0530

    auto wiring using xml

---
+++ src/main/java/com/springCore/auto/wire/Test.java
@@ -0,0 +1,16 @@
+package com.springCore.auto.wire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springCore/auto/wire/autowire_config.xml");
+		Employee emp1 = (Employee) context.getBean("emp");
+		System.out.println(emp1);
+
+	}
+
+}
